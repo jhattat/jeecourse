93a6803210c63a51fc24c76ba99de00724d387f9
Jérémie HATTAT <jhattat@gmail.com>
2012-09-30 23:16:48 +0200

    refactor

--- webserver/src/main/java/craftsmen/associates/basics/SimpleServer.java
+++ webserver/src/main/java/craftsmen/associates/basics/SimpleServer.java
@@ -1,6 +1,8 @@
 package craftsmen.associates.basics;
 
+import java.awt.HeadlessException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
@@ -8,6 +10,8 @@ import java.net.Socket;
 import java.util.concurrent.Executors;
 
 public class SimpleServer implements Runnable {
+	private static final String HTML_HEADER = "HTTP/1.0 200 OK\nMIME_version:1.0\nContent_Type:text/html\n";
+	private static final String HTML_CONTENT = "<html> <head></head><body> <h1> hi</h1></Body></html>";
 	private boolean stop = false;
 	private int port = 8111;
 
@@ -24,36 +28,36 @@ public class SimpleServer implements Runnable {
 
 	public void run() {
 		try {
-			ServerSocket ss = new ServerSocket(port);
+			ServerSocket server = new ServerSocket(port);
 			while (!stop) {
-				Socket s = ss.accept();
-				BufferedReader in = new BufferedReader(new InputStreamReader(
-						s.getInputStream()));
-				System.out.println("Reading request ");
-				String info = null;
-				while ((info = in.readLine()) != null) {
-					System.out.println("now got " + info);
-					if (info.equals(""))
-						break;
+				try (Socket s = server.accept()) {
+					read(s);
+					write(s);
 				}
-
-				PrintStream out = new PrintStream(s.getOutputStream());
-				System.out.println("Request read ");
-				out.println("HTTP/1.0 200 OK");
-				out.println("MIME_version:1.0");
-				out.println("Content_Type:text/html");
-				String c = "<html> <head></head><body> <h1> hi</h1></Body></html>";
-				out.println("Content_Length:" + c.length());
-				out.println("");
-				out.println(c);
-				out.println("");
-
-				// out.close();
-				// in.close(); // close the socket !
-				s.close();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private void write(Socket s) throws IOException {
+		PrintStream out = new PrintStream(s.getOutputStream());
+		System.out.println("Request read ");
+		out.print(HTML_HEADER);
+		String c = HTML_CONTENT;
+		out.println("Content_Length:" + c.length());
+		out.println("");
+		out.println(c);
+		out.println("");	}
+
+	private void read(Socket s) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		System.out.println("Reading request ");
+		String info = null;
+		while ((info = in.readLine()) != null) {
+			System.out.println("now got " + info);
+			if (info.equals(""))
+				break;
 		}
 	}
 
