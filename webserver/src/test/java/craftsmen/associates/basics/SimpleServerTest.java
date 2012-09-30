1043ba27415075f0ef5fe3a99312c739cedcbe0c
Jérémie HATTAT <jhattat@gmail.com>
2012-09-30 22:38:18 +0200

    refactor, SRP

--- webserver/src/test/java/craftsmen/associates/basics/SimpleServerTest.java
+++ webserver/src/test/java/craftsmen/associates/basics/SimpleServerTest.java
@@ -3,6 +3,7 @@ package craftsmen.associates.basics;
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -16,9 +17,10 @@ public class SimpleServerTest {
 	
 	private static final int PORT = 8111;
 	private SimpleServer server=new SimpleServer(PORT);
+	private URLConnection connection;
 
 	@Test
-	public void serverIsOK() throws Exception {
+	public void serverAnswersHi() throws Exception {
 		whenServerIsLaunched();
 		whenISendARequest();
 		thenAnswerContains("hi");
@@ -27,39 +29,47 @@ public class SimpleServerTest {
 
 	private void stopServer() {
 		server.stop();
-		
 	}
 
 	private void thenAnswerContains(String answerExpected) throws Exception {
+		assertTrue(readFromConection().contains(answerExpected));
+	}
+
+	private String readFromConection() throws IOException {
+		return readerToString(connection.getInputStream());
+	}
+
+	private static String readerToString(InputStream input)
+			throws IOException {
+		BufferedReader isr2 = new BufferedReader(new InputStreamReader(input));
+		
+		StringBuilder builder = new StringBuilder();
+		String read = "";
+		do{
+			read = isr2.readLine();
+			builder.append(read);
+		}while(isReadable(read));
+		return builder.toString();
+	}
+
+	private static boolean isReadable(String read) {
+		return (read != null && read.length()>1);
+	}
+
+	private URLConnection whenISendARequest() throws Exception {
 		URL url = new URL("http://localhost:"+PORT);
-		URLConnection connection = url.openConnection();
+		connection = url.openConnection();
+		writeInConnection();
+		
+		return connection;
+	}
+
+	private void writeInConnection() throws IOException {
 		connection.setDoOutput(true);
 		PrintStream printStream = new PrintStream(connection.getOutputStream());
 		printStream.println("GET / HTTP/1.1");
 		printStream.println("");
 		printStream.close();
-		
-		InputStream inputStream = connection.getInputStream();
-		BufferedReader isr = new BufferedReader(new InputStreamReader(inputStream));
-		StringBuilder builder = new StringBuilder();
-		String read = "";
-		do{
-			read = isr.readLine();
-			builder.append(read);
-		}while(isReadable(read));
-		
-		System.out.println(builder.toString());
-		assertTrue(builder.toString().contains(answerExpected));
-		
-	}
-
-	private boolean isReadable(String read) {
-		return (read != null && read.length()>1);
-	}
-
-	private void whenISendARequest() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	private void whenServerIsLaunched() throws Exception {
