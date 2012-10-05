6e775416f838bfa9ea24e6b4ab2b1725ac16ad60
Jérémie HATTAT <jhattat@gmail.com>
2012-10-05 22:25:26 +0200

    Utilisation d'un pool fr thread pour le serveur

--- webserver/src/main/java/craftsmen/associates/basics/SimpleServer.java
+++ webserver/src/main/java/craftsmen/associates/basics/SimpleServer.java
@@ -1,16 +1,19 @@
 package craftsmen.associates.basics;
 
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SimpleServer implements Runnable{
 	private boolean stop = false;
 	private ServerSocket server;
+	private ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(5);
 
 	public void run() {
 		try {
 			server = new ServerSocket(0);
 			while (!stop) {
-				new Thread(new HTTPRequest(server.accept())).start();
+				newFixedThreadPool.execute(new HTTPRequest(server.accept()));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
